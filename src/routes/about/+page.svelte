07d51fccf2e9046eb17cf02d7462d78bc213086a
Jii Eu <hello@jiieu.com>
2023-07-11 17:16:50 -0400

    about page implemented + pagination for home added

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,28 +1,78 @@
 <script>
   import Header from "/src/routes/Header.svelte"
+  import Cursor from '../Cursor.svelte'
+
+  let mouse = {x:0, y:0};
+  let currImage = 0;
+  let genBg;
+  let bodyBg;
+  const numberOfImages = 7;
+  $: preloadImageUrls = [...Array(numberOfImages).keys()].map((key) => `/gens/${key+1}.jpg`)
+  $: {
+    if(bodyBg) {
+      bodyBg.style.backgroundImage = `url('${preloadImageUrls[currImage]}')`
+    }
+    if(genBg) {
+      genBg.src = preloadImageUrls[currImage];
+    }
+  }
+
+  setInterval(() => {
+    currImage = (currImage + 1) % numberOfImages
+  }, 5000)
+
+    const handleMouseMove = (e) => {
+    mouse.x = e.clientX
+    mouse.y = e.clientY
+    document.querySelector('.cursor').style.top = `${mouse.y-10}px`
+    document.querySelector('.cursor').style.left = `${mouse.x-10}px`
+  }
+
+  const handleMouseOver = (e) => {
+    const c = document.querySelector('.cursor');
+    c.style.width = "35px";
+    c.style.height = "35px";
+  }
+  const handleMouseLeave = (e) => {
+    const c = document.querySelector('.cursor');
+    c.style.width = "20px";
+    c.style.height = "20px";
+  }
+
 </script>
 
-<main class='main-wrapper'>
+<svelte:head>
+  {#each preloadImageUrls as image}
+    <link rel="preload" as="image" href={image} />
+  {/each}
+</svelte:head>
+
+<Cursor />
+<main class='main-wrapper' on:mousemove={handleMouseMove}>
   <div class='wrapper'>
-    <a href="/">
+    <a href="/" on:mouseover={handleMouseOver} on:mouseleave={handleMouseLeave}>
       <img src="/asayake-logo.png" alt="logo" class="logo"/>
     </a>
-    <Header page="about"/>
+    <Header page="about" on:mouseover={handleMouseOver} on:mouseleave={handleMouseLeave}/>
   </div>
-  <div class='body-bg'>
+  <div bind:this="{bodyBg}" class='body-bg'>
     <div class="body">
-      <div class='title'>About Us</div>
+      <div class='title'>Who we are</div>
       <p>Asayake Taiko is a Japanese drumming group founded in the spring of 2002 at the University of California, San Diego campus.</p>
       <p>In spring of 2002, Bobby Koga, Reid Matsuoka and several other students came together to start a taiko group at the University of California, San Diego. With financial support from the university, the fledgling group purchased various equipment in order to construct the Japanese drums in garages of devoted members. In spring of 2004, Asayake Taiko at UCSD debuted with their completed drums at the 3rd Annual Nikkei Student Union Culture Show.</p>
       <p>Today, Asayake Taiko performs for on-campus cultural events, high school conferences, and various university functions. Likewise, Asayake Taiko aims to promote cultural awareness of Taiko to the greater San Diego area through performances and workshops at K-12 schools and other cultural functions.</p>
       <p>Each year, Asayake Taiko continues to grow in equipment and members to accomplish its mission statement: to promote cultural awareness not only in the UCSD campus community, but in the greater San Diego community as well.</p>
       <p>“Asayake” means “morning glow” in Japanese, and invokes the image of the early morning light playing on the ocean mist in La Jolla.</p>
     </div>
+      <div class='gen'>
+        <img bind:this={genBg} src="/gens/1.jpg" alt='gen'/>
+    </div>
   </div>
 </main>
 
 <style>
   main {
+    cursor: none;
     font-family: 'Roboto', sans-serif;
   }
 
@@ -38,38 +88,54 @@
   }
 
   .body {
-    max-width: 1280px;
-    margin: 0 auto;
+    border-radius: 100px 100px 0 0;
+    padding: 96px 0 0 256px;
     color: #fff;
     font-weight: 300;
+    height: 100vh;
+    backdrop-filter: blur(3px) brightness(0.35);
   }
 
   .body-bg {
     position: absolute;
     top: 160px;
     width: 100%;
-    height: 100vh;
     border-radius: 100px 100px 0 0;
     background-color: #791111;
-    padding-top: 48px;
-    box-shadow: 3px 0px 9px 3px #a0a0a0;
     transition: top 0.5s ease-in-out;
+    background-image: url('/gens/1.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
   }
 
   .body .title {
     font-size: 60px;
-    margin-bottom: 32px;
+    margin-bottom: 64px;
   }
 
   .body p {
-    width: 50%;
     line-height: 1.5;
     font-size: 18px;
+    width: 40%;
+    margin-bottom: 2rem;
   }
 
+  .gen img {
+    position: absolute;
+    right: 250px;
+    top: 250px;
+    width: 600px;
+    border-radius: 10px;
+    transition: transform 0.3s ease;
+  }
 
-  .our-name {
-    font-size: 40px;
+  .gen img:hover {
+    transform: scale(1.3);
+  }
+
+  a {
+    cursor: none !important;
   }
 
 </style>
