9b2b5da1526dee6e6518ea01020d930816507715
Jii Eu <hello@jiieu.com>
2023-07-14 12:49:24 -0400

    main images modified & about page somewhat done

--- src/routes/Cursor.svelte
+++ src/routes/Cursor.svelte
@@ -22,34 +22,23 @@
     const c2 = document.querySelector('.cursor-second');
     c2.style.transform = "translate(-50%, -50%) scale(1)"
   }
+
 </script>
 
 <svelte:document on:mousemove={handleMouseMove} on:mousedown={handleMouseDown} on:mouseup={handleMouseUp}/>
-<div class='cursor'></div>
-<div class='cursor-second'></div>
+<div class='cursor w-3 h-3 fixed z-[999] rounded-full bg-asa-red'></div>
+<div class='cursor-second w-3 h-3 fixed z-[999] rounded-full bg-asa-red bg-opacity-50'></div>
 
 <style>
 .cursor {
-  z-index: 999;
-  position: fixed;
-  border-radius: 999px;
   pointer-events: none;
-  width: 1rem;
-  height: 1rem;
-  background-color: #791111;
   transform: translate(-50%, -50%) scale(1);
   transition: opacity 0.15s ease-in-out 0s, transform 0.25s ease-in-out 0s;
 }
 
 .cursor-second {
-  z-index: 998;
-  position: fixed;
-  border-radius: 999px;
   pointer-events: none;
-  width: 0.6rem;
-  height: 0.6rem;
-  background-color: #e0baa177;
   transform: translate(-50%, -50%) scale(1);
-  transition: top 0.1s ease-in-out, left 0.1s ease-in-out, opacity 0.15s ease-in-out 0s, transform 0.25s ease-in-out 0s;
+  transition: opacity 0.15s ease-in-out 0s, transform 0.25s ease-in-out 0s;
 }
 </style>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Header from "/src/routes/Header.svelte"
+  import Footer from '../Footer.svelte'
+
+</script>
+
+<main class='main-wrapper'>
+  <Header page="contact" />
+</main>
+
+<svelte:head>
+  <title>Asayake Taiko | Contact Us</title>
+  <meta name="description" content="Asayake Taiko | Contact Us" />
+</svelte:head>
+
+<style>
+  main {
+    font-family: 'Poppins', sans-serif;
+  }
+</style>
