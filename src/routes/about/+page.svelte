3ab42c0b7c126cd33d227eaa558084fbae63e991
rolemadelen <rolemadelen17@pm.me>
2023-07-31 12:45:52 -0400

    Main page CSS modified

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -43,74 +43,12 @@
 
 <Cursor />
 <Header page="about" />
-<main class='main-wrapper'>
-  <div class='body-bg text-white font-light'>
-    <div class="bg-black flex justify-center items-center bg-opacity-80">
-      <div class='max-w-screen-xl grid auto-rows-min grid-cols-1 md:grid-cols-2 gap-4 m-auto px-8 py-20 '>
-        <div>
-          <div class='text-4xl font-medium mb-2'>Who we are</div>
-          <p class='mb-10'>
-            Asayake Taiko is a Japanese drumming group founded in the spring of 2002 at the University of California, San Diego campus. Our mission statement is to increase Japanese cultural awareness both within and outside of the UCSD community through taiko.
-          </p>
-          <div class='text-4xl font-medium mt-8 mb-2'>Our name</div>
-          <p>"Asayake" means "morning glow" in Japanese, and invokes the image of the early morning light playing on the ocean mist in La Jolla.</p>
-        </div>
-        <div role='button' tabindex='0' class='mt-8 md:mt-0 cursor-none relative m-auto' on:mouseover={handleMouseOver} on:mouseleave={handleMouseLeave} on:focus>
-          <picture>
-            <source srcset="/about/webp/3.webp" type="image/webp" />
-            <source srcset="/about/3.jpg" type="image/jpeg" />
-            <img class='w-full md:w-4/5 m-auto rounded-lg' src="/about/3.jpg" loading="lazy" decoding="async" alt='gen'/>
-          </picture>
-          <picture>
-            <source srcset="/about/webp/2.webp" type="image/webp" />
-            <source srcset="/about/2.jpg" type="image/jpeg" />
-            <img class='hidden-gen w-full md:w-4/5 mx-auto rounded-lg absolute right-[50%] top-[50%] translate-x-1/2 -translate-y-1/2 opacity-0 will-change-transform duration-300' src="/about/2.jpg" loading="lazy" decoding="async" alt='gen'/>
-          </picture>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class='body-bg2 text-white font-light'>
-    <div class="flex justify-center items-center pt-8 pb-20 bg-white opacity-[0.95] text-[#222]">
-      <div class='max-w-screen-xl grid auto-rows-min grid-cols-1 gap-4 m-auto px-8'>
-        <div class='text-4xl font-medium mt-10'>History</div>
-        <p>In spring of 2002, Bobby Koga, Reid Matsuoka and several other students came together to start a taiko group at the University of California, San Diego. With financial support from the university, the fledgling group purchased various equipment in order to construct the Japanese drums in garages of devoted members. In spring of 2004, Asayake Taiko at UCSD debuted with their completed drums at the 3rd Annual Nikkei Student Union Culture Show.</p>
-        <p>Today, Asayake Taiko performs for on-campus cultural events, high school conferences, and various university functions. Likewise, Asayake Taiko aims to promote cultural awareness of Taiko to the greater San Diego area through performances and workshops at K-12 schools and other cultural functions.</p>
-        <p>Each year, Asayake Taiko continues to grow in equipment and members to accomplish its mission statement: to promote cultural awareness not only in the UCSD campus community, but in the greater San Diego community as well.</p>
-        <div bind:this="{readMoreDiv}" class='max-h-0 overflow-hidden duration-500'>
-          <div>
-            <div class='text-4xl font-medium mb-4 mt-10'>Origin</div>
-            <div class='grid grid-cols-1 md:grid-cols-2'>
-              <p class='mb-8'>Taiko, meaning "drum", refers to a large family of Japanese percussive instruments. Traditional taiko had its use as a part of warfare, theater, festivals, and religious ceremonies. However, the art of taiko as we know it today can be traced back to Daihachi Oguchi in 1951. As a jazz drummer, Oguchi took traditional taiko music and arranged them to be performed as a group. This new ensemble style of taiko became known as kumi-daiko, which quickly spread to the rest of Japan and the world. Notable kumi-daiko ensembles that originated in Japan include Oguchi's own Osuwa Daiko, Oedo Sukeroku Daiko, Kodo, and Wadaiko Yamato.</p>
-              <picture>
-                <source srcset="/about/webp/oguchi.webp" type="image/webp" />
-                <source srcset="/about/oguchi.jpg" type="image/jpeg" />
-                <img class='rounded-lg m-auto w-4/5' src="/about/oguchi.jpg" alt="Daihachi Oguchi" />
-            </picture>
-            </div>
-          </div>
-          <div class='grid grid-cols-1 md:grid-cols-2 md:gap-14 mb-4 md:mb-8'>
-            <div>
-              <div class='text-4xl font-medium mb-4 mt-10'>Spread</div>
-              <p>Since its inception, kumi-daiko has become the dominant form of taiko performance outside of Japan. The spread of taiko in the United States started in the 1960s, when Seiichi Tanaka founded the San Francisco Taiko Dojo. This was closely followed by other groups such as Kinnara Taiko and San Jose Taiko. The taiko community continued to grow as groups began to be founded as university and college organizations. In 1990, Kyodo Taiko was founded on the campus of the University of California, Los Angeles, becoming the first collegiate taiko group.</p>
-            </div>
-            <div>
-              <div class='text-4xl font-medium mb-4 mt-10'>Today</div>
-              <p>The growth of the taiko community continues today, through the founding of new groups, as well as the art form receiving more mainstream attention. Taiko has been featured in performances such as Cirque du Soleil and at events such as the Academy Awards and the Grammy Awards. Professional ensembles have gone on to tour the entire world, bringing the art of taiko to a wider audience. Events such as the North American Taiko Conference and the Intercollegiate Taiko Invitational has further facilitated the growth of the taiko community by bringing together performers from all over the country in order to learn from one another.</p>
-            </div>
-          </div>
-        </div>
-        <button type='button' class="mt-4 btn w-32 rounded-lg text-sm shadow-asa-red shadow-sm m-auto" on:click={handleClick}>
-          <span>
-          Read More
-          </span>
-          <img src="/icons/down-arrow.svg" width="18" alt="down arrow" />
-        </button>
-      </div>
-    </div>
+<main class='main-wrapper bg-black'>
+  <video class="pc" src="/about/asayake-video-720.mp4" loop="true" autoplay="true" muted="true" playsinline="true"></video>
+  <div class='h-[10rem] items-end :h-full flex justify-center lg:items-center relative bottom-0 lg:absolute lg:top-1/2 lg:left-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2'>
+    <span class='text-white lg:font-semibold text-xl lg:text-6xl'>About Us</span>
   </div>
 </main>
-<Footer />
 
 <style>
   main {
@@ -120,12 +58,20 @@
   .body-bg,
   .body-bg2 {
     width: 100%;
-    background-color: #791111;
+    background-color: #000;
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
   }
   .body-bg {
-    background-image: url('/about/webp/1.webp');
+    xbackground-image: url('/about/webp/1.webp');
+  }
+  video {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    margin: auto auto;
+    z-index: -1;
+    filter: brightness(70%);
   }
 </style>
