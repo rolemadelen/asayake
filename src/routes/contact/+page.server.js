b3ad712675d416aa3ba35356dac26882c62c1469
Madelen Yoo <rolemadelen@tuta.io>
2023-08-14 19:45:53 -0400

    sendgrid really fixed..
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,17 +1,18 @@
 import sendgrid from '@sendgrid/mail';
 import { redirect, fail } from '@sveltejs/kit';
+import { SENDGRID_API_KEY } from '$lib/env.js';
 
 export const actions = {
 	default: async ({ request, cookies }) => {
 		const data = await request.formData();
 
         // for spam protection
-        // if(data.get('honey') !== '') {
-        //     return fail(403, {incorrect: true})
-        // }
+        if(data.get('honey') !== '') {
+            return fail(403, {incorrect: true})
+        }
 
-        sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-
+        sendgrid.setApiKey(SENDGRID_API_KEY)
+        
         const msg = {
           to: 'ryuj0415@gmail.com', // Change to your recipient
           from: 'ryuj0415@gmail.com', // Change to your verified sender
@@ -20,6 +21,13 @@ export const actions = {
           html: `Sent from Asayake Contact Page. <br /><br /> <b>Name</b>: ${data.get('name')} <br /> <b>Phone (optional)</b>: ${data.get('phone')} <br /> <b>Email</b>: ${data.get('email')} <br /> <b>Organization/Event</b>: ${data.get('organization')} <br /> <b>Date</b>: ${data.get('date')} <br /> <b>Time</b>: ${data.get('time')} <br /><br /> <b>Detail</b>: <pre>${data.get('detail')}</pre>`,
         }
 
-        sendgrid.send(msg)
+        try {
+          const sended = await sendgrid.send(msg);
+          console.log("sended: ", sended);
+          return { success: true };
+        } catch(err) {
+          console.log(err);
+          return { success: false };
+        }
 	}
 };
