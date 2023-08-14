1726ac50713165a07454ea15825eb68f3144cccc
Madelen Yoo <rolemadelen@tuta.io>
2023-08-14 19:36:49 -0400

    sendgrid fix...
    Signed-off-by: Jii Yoo <jiiyoo@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@pm.me>
    Signed-off-by: Madelen Yoo <rolemadelen@tuta.io>

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -1,6 +1,5 @@
 import sendgrid from '@sendgrid/mail';
 import { redirect, fail } from '@sveltejs/kit';
-import { SENDGRID_API_KEY } from '$lib/env.js';
 
 export const actions = {
 	default: async ({ request, cookies }) => {
@@ -11,7 +10,8 @@ export const actions = {
             return fail(403, {incorrect: true})
         }
 
-        sendgrid.setApiKey(SENDGRID_API_KEY)
+        sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
         const msg = {
           to: 'ryuj0415@gmail.com', // Change to your recipient
           from: 'ryuj0415@gmail.com', // Change to your verified sender
@@ -20,14 +20,6 @@ export const actions = {
           html: `Sent from Asayake Contact Page. <br /><br /> <b>Name</b>: ${data.get('name')} <br /> <b>Phone (optional)</b>: ${data.get('phone')} <br /> <b>Email</b>: ${data.get('email')} <br /> <b>Organization/Event</b>: ${data.get('organization')} <br /> <b>Date</b>: ${data.get('date')} <br /> <b>Time</b>: ${data.get('time')} <br /><br /> <b>Detail</b>: <pre>${data.get('detail')}</pre>`,
         }
 
-        sendgrid
-          .send(msg)
-          .then(() => {
-            console.log('Email sent')
-            throw redirect(303, '/contact')
-          })
-          .catch((error) => {
-            return fail(403, {incorrect: true})
-          })
+        sendgrid.send(msg)
 	}
 };
